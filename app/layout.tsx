3d3f084d3763e9527bd941c3b511a39e76342609
Muhammad Sami Ullah <munda295@outlook.com>
2025-09-03 18:52:21 +0500

    Fix hydration error in theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,7 +106,7 @@ export default function RootLayout({
         />
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange={false}
         >
